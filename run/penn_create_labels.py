973b31b0da098a70ce638059cc1731d6031d6b9a
Roger Leitzke <roger.leitzke@gmail.com>
2018-07-30 11:03:43 -0300

    Extract the content from Penn dataset

--- run/penn_create_labels.py
+++ run/penn_create_labels.py
@@ -21,38 +21,39 @@ from os.path import dirname, join, exists
 from datasets import pennaction
 from classes import filehandler as fh
 
-def main(inputfolder, imsize, output=None):
+def main(datainput, imsize, output=None):
     """
     Parameters:
     -----------
-    inputfolder : string
-        path to the root folder containing the dataset
+    datainput : string
+        path to the root folder or file containing the dataset
     imsize : int
         size of the new images
     output : string
         path to the folder where the new dataset will be saved
     """
-    inputfolder = fh.is_folder(inputfolder)
-    if output:
-        dirout = fh.is_folder(output)
+    input = fh.is_folder(datainput, boolean=True)
+    if input:
+        # create a pathfile to the dataset
+        pennaction.create_pathfile(input)
     else:
-        dirout = join(inputfolder, str(imsize))
-        if not exists(dirout):
-            os.makedirs(dirout)
-    # create a pathfile to the dataset
-    pennaction.create_pathfile(inputfolder)
-    # resize dataset to the new path
-    #pennaction.resize_dataset(inputfolder, 
-
-
+        input = fh.is_file(datainput, boolean=True)
+        if output:
+            dirout = fh.is_folder(output)
+        else:
+            dirout = join(dirname(input), str(imsize))
+            if not exists(dirout):
+                os.makedirs(dirout)
+    
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('inputfolder', metavar='folder_input', 
-                        help='path to the folder containing Penn dataset')    
+    parser.add_argument('input', metavar='input', 
+                        help='path to the file or folder containing Penn dataset')    
     parser.add_argument('imsize', metavar='image_size', type=int, default=256,
                         help='new size to the images')
     parser.add_argument('-o', '--output', metavar='output_folder', default=None,
                         help='directory to save the output files')
     args = parser.parse_args()
 
-    main(args.inputfolder, args.imsize, output=args.output)
+    main(args.input, args.imsize, output=args.output)
